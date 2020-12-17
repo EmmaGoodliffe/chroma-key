848ad6c055200a05910aec43b499b5bf230058dd
Emma Goodliffe <emma@piemag.com>
2020-12-17 15:02:58 +0000

    done

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,9 @@
   import { colour1, colour2 } from "./store";
 
   let files: FileList;
-  let preview: string;
+  let preview = "";
+
+  $: buttonIsDisabled = !(files && files.length);
 
   const post = async (url: string, data: Object) => {
     const response = await fetch(url, {
@@ -46,26 +48,58 @@
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
+  @mixin btn {
+    padding: 0.5rem 1rem;
+    background-color: #eee;
+    border: none;
+    border-radius: 0.2rem;
+  }
   main {
     display: flex;
     flex-direction: column;
     align-items: center;
+    width: 80%;
+    margin: auto;
     div {
-      margin: 10px 0;
       flex: 1;
+      margin: 1rem 0;
+      &.instructions {
+        font-style: italic;
+        text-decoration: underline;
+        text-underline-position: under;
+      }
+      input[type="file"] {
+        color: white;
+        &::-webkit-file-upload-button {
+          @include btn;
+        }
+      }
       button {
-        padding: 10px 20px;
-        font-weight: bold;
-        background-color: #eee;
+        @include btn;
+        &.disabled {
+          cursor: not-allowed;
+          opacity: 0.5;
+        }
       }
     }
   }
 </style>
 
 <main>
-  <div><input type="file" bind:files /></div>
-  <ColourPicker onUpdate={c => colour1.set(c)} />
-  <ColourPicker onUpdate={c => colour2.set(c)} />
-  <button on:click={chromaKey}>Chroma key</button>
-  <div><img src={preview} alt="Preview" /></div>
+  <div class="instructions">Choose an image</div>
+  <div><input type="file" id="image-input" bind:files /></div>
+  <div class="instructions">Choose how to convert colours</div>
+  <div>
+    <ColourPicker title="Convert from..." onUpdate={c => colour1.set(c)} />
+    <ColourPicker title="...to..." onUpdate={c => colour2.set(c)} />
+  </div>
+  <div class="instructions">Go!</div>
+  <div>
+    <button
+      class:disabled={buttonIsDisabled}
+      on:click={buttonIsDisabled ? null : chromaKey}>Chroma key</button>
+  </div>
+  <div>
+    {#if preview.length}<img src={preview} alt="Preview" />{/if}
+  </div>
 </main>
--- src/ColourPicker.svelte
+++ src/ColourPicker.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { defaultHex, defaultAlpha } from "./store";
 
+  export let title: string;
   export let onUpdate: (colour: string) => void;
 
   let hex = defaultHex;
@@ -10,12 +11,45 @@
 </script>
 
 <style lang="scss">
-  .preview {
-    width: 100px;
-    height: 100px;
+  $length: 8rem;
+  .colour-picker {
+    display: flex;
+    width: 100%;
+    font-size: 1.5rem;
+    .title {
+      min-width: 12rem;
+      text-align: right;
+    }
+    .preview {
+      flex: 1;
+      margin: auto 1rem;
+      min-width: $length;
+      max-width: $length;
+      border: 0.1rem solid white;
+      text-align: center;
+      font-family: monospace;
+      line-height: 2rem;
+    }
+    input {
+      flex: 1;
+      &[type="text"] {
+        background-color: transparent;
+        border: 0.1rem solid white;
+        color: white;
+        font-size: inherit;
+        min-width: $length;
+        max-width: $length;
+      }
+    }
   }
 </style>
 
-<input type="color" bind:value={hex} on:change={() => onUpdate(fullColour)} />
-<input type="text" bind:value={alpha} on:change={() => onUpdate(fullColour)} />
-<div class="preview" style={`background-color: ${fullColour};`} />
+<div class="colour-picker">
+  <span class="title">{title}</span>
+  <div class="preview" style={`color: ${fullColour};`}>{fullColour}</div>
+  <input type="color" bind:value={hex} on:change={() => onUpdate(fullColour)} />
+  <input
+    type="text"
+    bind:value={alpha}
+    on:change={() => onUpdate(fullColour)} />
+</div>
